fca8474865f066c2972039e9ba0bb756a5b2b22a
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-19 19:05:49 +0000

    Add search bar collapse

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,18 @@
+function checkToTop() {
+    $('#to-top').removeClass('visible');
+    if (window.scrollY) {
+        $('#to-top').addClass('visible');
+    }
+}
+
 $(document).ready(() => {
     // Makes the "Back to Top" button only visible when the user has scrolled past the start of the page
-    $(window).on('scroll', () => {
-        $('#to-top').removeClass('visible');
-        if (window.scrollY) {
-            $('#to-top').addClass('visible');
-        }
-    });
+    $(window).on('scroll', checkToTop);
 
     // Returns to the top of the screen when the "Back to Top" button is clicked
     $('#to-top').click(function() {
         window.scrollTo(0, 0);
     });
+
+    checkToTop();
 });
