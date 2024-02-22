8b3ec9fe8466625d4bd60b592d9943bd0a6d1b52
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-22 16:23:14 +0000

    Fix overlap with brand logo and content body

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Scrolls to the top of the screen, and hides the "Back to Top" button
+ */
 function scrollScreen() {
     $('#to-top').removeClass('visible');
     if (window.scrollY) {
@@ -9,9 +12,29 @@ function scrollScreen() {
     $('#hero-image').css('background-position-y', `${heroImagePosition}px`);
 }
 
+/**
+ * Adjusts elements whose properties depend on the screen width
+ */
+function resizeWindow() {
+    let width = $(document).width();
+
+    // Creates sufficient spacing between the brand logo and main page body to avoid overlap
+    let logoHeight = $('#logo-brand').height();
+    let navHeight = $('#navbar').height();
+    let headingGap = Math.ceil((logoHeight / 2) - (navHeight / 2));
+    if (width < 768) {
+        headingGap -= navHeight;
+        console.log('smol');
+    }
+    $('#content-header').css('min-height', '');
+    if (headingGap > 0) {
+        $('#content-header').css('min-height', `${headingGap}px`);
+    }
+}
+
 $(document).ready(() => {
     // Makes the "Back to Top" button only visible when the user has scrolled past the start of the page
-    $(window).on('scroll', scrollScreen);
+    $(window).on('scroll', scrollScreen).resize(resizeWindow);
 
     // Returns to the top of the screen when the "Back to Top" button is clicked
     $('#to-top').click(function() {
@@ -19,4 +42,5 @@ $(document).ready(() => {
     });
 
     scrollScreen();
+    resizeWindow();
 });
