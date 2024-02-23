63614c8b871fec8629852094eeb9f5ef5db7cd46
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-23 12:04:44 +0000

    Set up products.css file

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from core.contexts import get_base_context
 from .models import PastryProduct
+from django.conf import settings
 
 # Create your views here.
 
@@ -11,4 +12,5 @@ class ProductList(View):
     def get(self, request):
         context = get_base_context(request)
         context['products'] = PastryProduct.objects.all()
+        print(settings.STATICFILES_DIRS)
         return render(request, self.template, context)
