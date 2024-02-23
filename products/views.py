045f7316e359dad93a5f03f10f508c513deb0e32
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-23 17:16:45 +0000

    Add product list pagination main structure

--- products/views.py
+++ products/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.views import View
+from django.views import generic, View
 from core.contexts import get_base_context
 from .models import PastryProduct
 from django.conf import settings
 
 # Create your views here.
 
-class ProductList(View):
-    template = 'products/product_list.html'
+class ProductList(generic.ListView):
+    model = PastryProduct
+    queryset = PastryProduct.objects.all()
+    template_name = 'products/product_list.html'
+    paginate_by = 48
 
-    def get(self, request):
-        context = get_base_context(request)
-        context['products'] = PastryProduct.objects.all()
-        print(settings.STATICFILES_DIRS)
-        return render(request, self.template, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(get_base_context(self.request))
+        return context
