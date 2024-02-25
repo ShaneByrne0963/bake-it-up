b612c82c8ee0c252b37fb0d1b84625b43fbaa2fd
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-25 17:59:26 +0000

    Update navigation to filter products by category

--- products/views.py
+++ products/views.py
@@ -7,12 +7,24 @@ from django.conf import settings
 # Create your views here.
 
 class ProductList(generic.ListView):
-    model = PastryProduct
-    queryset = PastryProduct.objects.all()
     template_name = 'products/product_list.html'
+    context_object_name = 'products'
     paginate_by = 48
+
+    def get_queryset(self):
+        category = 'all'
+        if 'category' in self.request.GET:
+            category = self.request.GET['category']
+        if category == 'all':
+            return PastryProduct.objects.all()
+        else:
+            return PastryProduct.objects.filter(category__name=category)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(get_base_context(self.request))
+
+        if 'category' in self.request.GET:
+            category = self.request.GET['category']
+            context['get_url'] = f'category={category}'
         return context
