66756af30fe2f973d8090a8933a8fb9e2f42ef1d
Shane Byrne <shanebyrne0963@gmail.com>
2024-02-26 17:29:43 +0000

    Fix out of place logo for size large screens

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductList.as_view(), name='product_list'),
-    path('/<str:product_name>', views.ProductDetail.as_view(),
+    path('<str:product_name>', views.ProductDetail.as_view(),
          name='product_detail'),
 ]
