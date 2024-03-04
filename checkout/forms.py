b2c420276d387f94b1b51873de3229e5afad8376
Shane Byrne <shanebyrne0963@gmail.com>
2024-03-04 17:26:35 +0000

    Add checkout form class

---
+++ checkout/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class CheckoutForm(forms.Form):
+
+    name = forms.CharField(label="Name/Company Name",
+                           max_length=70,
+                           required=True)
+    email = forms.EmailField(label="Email Address",
+                             max_length=320,
+                             required=True)
+    phone = forms.CharField(label="Phone Number",
+                            max_length=20,
+                            required=True)
--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from .forms import CheckoutForm
 from core.contexts import get_base_context
 
 
 class Checkout(View):
     template = 'checkout/checkout.html'
 
-    def get(self, request):
+    def post(self, request):
         context = get_base_context(request)
+        context['form'] = CheckoutForm()
 
         return render(request, self.template, context)
