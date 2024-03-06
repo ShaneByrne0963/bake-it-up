e59056f3cc5398a66c4b12259261579279aa9070
Shane Byrne <shanebyrne0963@gmail.com>
2024-03-06 18:26:09 +0000

    Add form feedback messages to payment modal

--- checkout/static/checkout/js/checkout.js
+++ checkout/static/checkout/js/checkout.js
@@ -1,32 +1,55 @@
+/**
+ * Displays any errors found in the user's payment details
+ * @param {Event} event The event handler
+ * @param {String} errorID The ID of the error display div
+ */
+function handlePaymentErrors(event, errorID) {
+    let errorDiv = $(errorID);
+    errorDiv.text('');
 
-$(document).ready(() => {
-    let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-    let clientSecret = $('#id_client_secret').text().slice(1, -1);
+    if (event.error) {
+        errorDiv.text(event.error.message);
+    }
+}
 
-    let stripe = Stripe(stripePublicKey);
-    let elements = stripe.elements();
 
-    let style = {
-        base: {
-            color: '#495057',
-            fontFamily: '"Roboto", sans-serif',
-            fontSmoothing: 'antialiased',
-            fontSize: '17px',
-            '::placeholder': {
-                color: '#a9a9a9'
-            }
-        },
-        invalid: {
-            color: '#dc3545',
-            iconColor: '#dc3545'
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+
+let stripe = Stripe(stripePublicKey);
+let elements = stripe.elements();
+
+let style = {
+    base: {
+        color: '#495057',
+        fontFamily: '"Roboto", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '17px',
+        '::placeholder': {
+            color: '#a9a9a9'
         }
-    };
+    },
+    invalid: {
+        color: '#495057',
+        iconColor: '#dc3545'
+    }
+};
 
-    let cardNumber = elements.create('cardNumber', {style: style});
-    let cardExpiry = elements.create('cardExpiry', {style: style});
-    let cardCvc = elements.create('cardCvc', {style: style});
+let cardNumber = elements.create('cardNumber', {style: style});
+let cardExpiry = elements.create('cardExpiry', {style: style});
+let cardCvc = elements.create('cardCvc', {style: style});
 
-    cardNumber.mount('#card-number');
-    cardExpiry.mount('#card-expiry');
-    cardCvc.mount('#card-cvc');
+cardNumber.mount('#card-number');
+cardExpiry.mount('#card-expiry');
+cardCvc.mount('#card-cvc');
+
+// Card Error handling
+cardNumber.addEventListener('change', function(event) {
+    handlePaymentErrors(event, '#card-number-errors');
+});
+cardExpiry.addEventListener('change', function(event) {
+    handlePaymentErrors(event, '#card-expiry-errors');
+});
+cardCvc.addEventListener('change', function(event) {
+    handlePaymentErrors(event, '#card-cvc-errors');
 });
