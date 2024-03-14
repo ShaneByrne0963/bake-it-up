3742196a953835cfa7bbf24d4c0916cfc8d3d606
Shane Byrne <shanebyrne0963@gmail.com>
2024-03-14 10:17:23 +0000

    Update profile page to create user profile if none found

--- profiles/models.py
+++ profiles/models.py
@@ -13,3 +13,19 @@ class UserProfile(models.Model):
     saved_town_or_city = models.CharField(max_length=60, blank=True, null=True)
     saved_county = models.CharField(max_length=10, blank=True, null=True)
     saved_postcode = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.get_user_name()}'s Profile"
+
+    def get_user_name(self):
+        """
+        Returns the user's name, or email if a name is not entered
+        """
+        name = ''
+        if self.user.first_name:
+            name = self.user.first_name
+            if self.user.last_name:
+                name += f' {self.user.last_name}'
+        else:
+            name = self.user.email
+        return name
--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.views import View
 
 from core.contexts import get_base_context
+from .models import UserProfile
 
 
 class AccountSettings(View):
@@ -24,6 +25,30 @@ class AccountSettings(View):
                 'login_custom_redirect': 'account_settings'
             }
             return redirect('home')
-
         context = get_base_context(request)
+
+        # Getting the user profile, or creating one if none exists
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            contact_details = {
+                'saved_first_name': request.user.first_name,
+                'saved_last_name': request.user.last_name,
+                'saved_phone_number': profile.saved_phone_number,
+            }
+            billing_details = {
+                'saved_street_address1': profile.saved_street_address1,
+                'saved_street_address2': profile.saved_street_address2,
+                'saved_town_or_city': profile.saved_town_or_city,
+                'saved_county': profile.saved_county,
+                'saved_postcode': profile.saved_postcode,
+            }
+            context['contact_details'] = contact_details
+        except UserProfile.DoesNotExist:
+            profile = UserProfile.objects.create(user=request.user)
+            messages.warning(
+                request,
+                """Your account details are missing. Any data previously
+                entered has been reset."""
+            )
+
         return render(request, self.template, context)
