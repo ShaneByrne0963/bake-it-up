2c87f6f214541ea4ba1eb0870cb9606b21d5f62e
Shane Byrne <shanebyrne0963@gmail.com>
2024-03-22 11:01:18 +0000

    Add ability to send customer messages

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib import messages
 
 from core.contexts import get_base_context
 from .forms import CustomerMessageForm
@@ -10,7 +11,35 @@ class StoreContact(View):
 
     def get(self, request):
         context = get_base_context(request)
-        contact_form = CustomerMessageForm()
+
+        # Get the user's name and email
+        user_info = {}
+        if request.user.is_authenticated:
+            user_info['email'] = request.user.email
+            if request.user.first_name:
+                full_name = request.user.first_name
+                if request.user.last_name:
+                    full_name += f' {request.user.last_name}'
+                user_info['full_name'] = full_name
+
+        contact_form = CustomerMessageForm(initial=user_info)
         context['contact_form'] = contact_form
 
         return render(request, self.template, context)
+    
+    def post(self, request):
+        """
+        Sends the customer message
+        """
+        contact_form = CustomerMessageForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, "Your message has been sent! \
+                We will get back to you shortly.")
+            return redirect('home')
+        else:
+            messages.error(request, "Your contact details are invalid. \
+                Please double check your details and try again.")
+            context = get_base_context(request)
+            context['contact_form'] = contact_form
+            return render(request, self.template, context)
