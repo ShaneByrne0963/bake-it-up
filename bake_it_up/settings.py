51079ac66cc7edf285e85175f6c8e00f572b2694
Shane Byrne <shanebyrne0963@gmail.com>
2024-03-25 19:44:04 +0000

    Set up Amazon Web Services for deployment

--- bake_it_up/settings.py
+++ bake_it_up/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'crispy_forms',
     'core',
     'home',
     'products',
@@ -58,6 +57,9 @@ INSTALLED_APPS = [
     'profiles',
     'contact',
     'orders',
+
+    'crispy_forms',
+    'storages'
 ]
 
 
@@ -221,6 +223,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Amazon Web Services
+
+if USE_AWS in os.environ:
+    # AWS Bucket Configuration
+    AWS_STORAGE_BUCKET_NAME = "bake-it-up-e014260e4351"
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
